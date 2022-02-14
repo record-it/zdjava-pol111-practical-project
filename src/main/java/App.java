a34a882169f93ffcef16642358002f9d4fc310d3
cs <siwon.cezary@gmail.com>
2022-02-14 20:57:41 +0100

    end of day 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nbpapi.Rate;
 import nbpapi.RateTable;
 import nbpapi.Table;
 import nbpapi.URIGenerator;
@@ -17,7 +18,7 @@ public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URIGenerator.currentTableJson(Table.TABLE_B))
+                .uri(URIGenerator.currentTableJson(Table.TABLE_A))
                 .GET()
                 .build();
         final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -25,6 +26,13 @@ public class App {
         final List<RateTable> rateTableList = mapper.readValue(response.body(), new TypeReference<List<RateTable>>() {
         });
         RateTable rateTable = rateTableList.get(0);
-        System.out.println(rateTable.getEffectiveDate());
+        System.out.println("Tabela: \t\t" + rateTable.getTable());
+        System.out.println("Numer: \t\t\t" + rateTable.getNo());
+        System.out.println("Data ważności: \t" + rateTable.getEffectiveDate());
+        System.out.println("=========================================================");
+        for (Rate rate: rateTable.getRates()) {
+            System.out.println(String.format("%-35s %5s %15.4f", rate.getCurrency(), rate.getCode(), rate.getMid()));
+        }
+
     }
 }
